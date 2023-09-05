c7843aef2cd845f85bcc4eea3d6937d1bf651894
annKlush <109134951+annKlush@users.noreply.github.com>
2023-09-05 20:43:50 +0300

    Dockerfile

---
+++ telegram-bot/src/main/java/com/example/telegrambot/Constant.java
@@ -0,0 +1,14 @@
+package com.example.telegrambot;
+
+public class Constant {
+    private static String BOT_NAME = "Java_Mate_Vacancies_bot";
+    private static String BOT_TOKEN = "6642036638:AAFYO1aisxTcSeHr6UpntV_URYEjcYBwppY";
+
+    public static String getBotName() {
+        return BOT_NAME;
+    }
+
+    public static String getBotToken() {
+        return BOT_TOKEN;
+    }
+}
